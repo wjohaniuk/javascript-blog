5b62052fee44f2e39a8ea155eb797d92fd9b2445
Wojciech Johaniuk <wojciech.johaniuk@gmail.com>
2025-11-27 17:14:02 +0100

    Add script to recognize clicked element

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const posts = document.querySelectorAll('.post');
+
+function titleClickHandler() {
+	for (let post of posts) {
+        post.classList.remove('active');
+    }
+ 
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
+	link.addEventListener('click', titleClickHandler);
+}
