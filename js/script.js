7e6f474dce85429338d9d0db9ff53fa6b3dff6d0
Wojciech Johaniuk <wojciech.johaniuk@gmail.com>
2025-11-27 17:56:24 +0100

    add article selection

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 'use strict';
 
+const links = document.querySelectorAll('.titles a');
 const posts = document.querySelectorAll('.post');
 
-function titleClickHandler() {
+function titleClickHandler(event) {
+	event.preventDefault();
+	const clickedElement = this;
 	for (let post of posts) {
-        post.classList.remove('active');
-    }
- 
+		post.classList.remove('active');
+	}
+	for (let link of links) {
+		link.classList.remove('active');
+	}
+	clickedElement.classList.add('active');
+	const articleSelector = clickedElement.getAttribute('href');
+	const targetArticle = document.querySelector(articleSelector);
+	targetArticle.classList.add('active');
 }
-
-const links = document.querySelectorAll('.titles a');
 
 for (let link of links) {
 	link.addEventListener('click', titleClickHandler);
