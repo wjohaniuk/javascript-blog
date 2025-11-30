5bf7ddea56187b063ac06ed4a76deffe363f693f
Wojciech Johaniuk <wojciech.johaniuk@gmail.com>
2025-11-30 21:48:06 +0100

    fix bug

--- js/script.js
+++ js/script.js
@@ -1,23 +1,45 @@
 'use strict';
 
-const links = document.querySelectorAll('.titles a');
 const posts = document.querySelectorAll('.post');
+const titleList = document.querySelector('.titles');
 
 function titleClickHandler(event) {
 	event.preventDefault();
 	const clickedElement = this;
+
 	for (let post of posts) {
 		post.classList.remove('active');
 	}
+	const links = titleList.querySelectorAll('a');
 	for (let link of links) {
 		link.classList.remove('active');
 	}
+
 	clickedElement.classList.add('active');
 	const articleSelector = clickedElement.getAttribute('href');
 	const targetArticle = document.querySelector(articleSelector);
-	targetArticle.classList.add('active');
+	if (targetArticle) {
+		targetArticle.classList.add('active');
+	}
 }
 
-for (let link of links) {
-	link.addEventListener('click', titleClickHandler);
+function generateTitleLinks() {
+	console.log('Generating title links...');
+	let html = '';
+	titleList.innerHTML = '';
+	for (let post of posts) {
+		const postId = post.getAttribute('id');
+		const postTitle = post.querySelector('.post-title').innerHTML;
+		const linkHTML =
+			'<li><a href="#' + postId + '"><span>' + postTitle + '</span></a></li>';
+		html += linkHTML;
+	}
+	titleList.innerHTML = html;
+
+	const links = titleList.querySelectorAll('a');
+	for (let link of links) {
+		link.addEventListener('click', titleClickHandler);
+	}
 }
+
+generateTitleLinks();
