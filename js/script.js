e2e5a9033685ce9edaf2198fc03a152a8c9c9407
unknown <Majster.PSE@LNCWA1I05390.ad.pse.pl>
2025-12-03 11:29:58 +0100

    add eslint

--- js/script.js
+++ js/script.js
@@ -4,42 +4,42 @@ const posts = document.querySelectorAll('.post');
 const titleList = document.querySelector('.titles');
 
 function titleClickHandler(event) {
-	event.preventDefault();
-	const clickedElement = this;
+  event.preventDefault();
+  const clickedElement = this;
 
-	for (let post of posts) {
-		post.classList.remove('active');
-	}
-	const links = titleList.querySelectorAll('a');
-	for (let link of links) {
-		link.classList.remove('active');
-	}
+  for (let post of posts) {
+    post.classList.remove('active');
+  }
+  const links = titleList.querySelectorAll('a');
+  for (let link of links) {
+    link.classList.remove('active');
+  }
 
-	clickedElement.classList.add('active');
-	const articleSelector = clickedElement.getAttribute('href');
-	const targetArticle = document.querySelector(articleSelector);
-	if (targetArticle) {
-		targetArticle.classList.add('active');
-	}
+  clickedElement.classList.add('active');
+  const articleSelector = clickedElement.getAttribute('href');
+  const targetArticle = document.querySelector(articleSelector);
+  if (targetArticle) {
+    targetArticle.classList.add('active');
+  }
 }
 
 function generateTitleLinks() {
-	console.log('Generating title links...');
-	let html = '';
-	titleList.innerHTML = '';
-	for (let post of posts) {
-		const postId = post.getAttribute('id');
-		const postTitle = post.querySelector('.post-title').innerHTML;
-		const linkHTML =
-			'<li><a href="#' + postId + '"><span>' + postTitle + '</span></a></li>';
-		html += linkHTML;
-	}
-	titleList.innerHTML = html;
+  console.log('Generating title links...');
+  let html = '';
+  titleList.innerHTML = '';
+  for (let post of posts) {
+    const postId = post.getAttribute('id');
+    const postTitle = post.querySelector('.post-title').innerHTML;
+    const linkHTML =
+      '<li><a href="#' + postId + '"><span>' + postTitle + '</span></a></li>';
+    html += linkHTML;
+  }
+  titleList.innerHTML = html;
 
-	const links = titleList.querySelectorAll('a');
-	for (let link of links) {
-		link.addEventListener('click', titleClickHandler);
-	}
+  const links = titleList.querySelectorAll('a');
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
 }
 
 generateTitleLinks();
